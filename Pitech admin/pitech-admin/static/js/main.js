f2785ef29599521657996b49f996b56a0c992429
Abhishek Kushwaha <abhishekkushwaha7928@gmail.com>
2026-03-25 15:46:42 +0530

    Initial commit - Pitech website update

---
+++ Pitech admin/pitech-admin/static/js/main.js
@@ -0,0 +1,7 @@
+// Main JS for PiTech Admin Panel
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('PiTech Admin Panel initialized');
+});
+
+// Any additional common JS functionality can go here
